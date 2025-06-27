09e1777c6394caa629f62f7200406e4a2cf05600
i_code <fullstack_61@proton.me>
2025-06-27 13:02:34 -0400

    extracted and formatted  necessary data

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,5 +1,6 @@
 
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -8,4 +9,19 @@ def emotion_detector(text_to_analyze):
 
     response = requests.post(url, json=my_input, headers=header)
 
-    return response.text
+    response_dict = json.loads(response.text)
+
+    emotions = response_dict['emotionPredictions'][0]['emotion']
+
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    result = {
+            'anger': emotions['anger'],
+            'disgust': emotions['disgust'],
+            'fear': emotions['fear'],
+            'joy': emotions['joy'],
+            'sadness': emotions['sadness'],
+            'dominant_emotion': dominant_emotion
+    }
+
+    return result
