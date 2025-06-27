fc1fcbcdff638b04c26178add0c66ab8dd804507
i_code <fullstack_61@proton.me>
2025-06-27 12:40:16 -0400

    Created emotion_detection file with emotion_detector func

---
+++ emotion_detection.py
@@ -0,0 +1,11 @@
+
+import requests
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    my_input = { "raw_document": { "text": text_to_analyze } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    response = requests.post(url, json=my_input, headers=header)
+
+    return response.text
